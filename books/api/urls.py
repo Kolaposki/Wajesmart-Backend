1f2453aa83383f2b35731e118ce690b6ac024559
Kolaposki <oshodikolapo@gmail.com>
2022-09-07 14:46:56 +0100

    :sparkles: Create book url path

--- books/api/urls.py
+++ books/api/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from books.views import *
+from books.views import BookView
 
-# from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
+    # Authors
+    # path("authors/", AuthorView.as_view()),
+    # path("author/<int:pk>", AuthorView.as_view()),
 
+    # Books
+    path("books/", BookView.as_view(), name="books"),
+    path("book/<int:pk>", BookView.as_view(), name="book"),
 ]
