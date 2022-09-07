8641c151b302aa867cf6a5500061a07422e23843
Kolaposki <oshodikolapo@gmail.com>
2022-09-07 15:51:02 +0100

    :art: Refactor models

--- books/models.py
+++ books/models.py
@@ -10,6 +10,7 @@ class Author(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
+    @property
     def full_name(self):
         return self.first_name + " " + self.last_name
 
@@ -21,7 +22,7 @@ class Author(models.Model):
 
 class Book(models.Model):
     name = models.TextField(null=False, blank=False)
-    isbn = models.TextField(null=False, blank=False) # isbn is 13 + 4 hyphens/space making 17 ==> (978-3-16-148410-0)
+    isbn = models.TextField(null=False, blank=False)  # isbn is 13 + 4 hyphens/space making 17 ==> (978-3-16-148410-0)
     author = models.ForeignKey(Author, related_name="author_books", on_delete=models.CASCADE)
     created_at = models.DateTimeField(null=True, auto_now_add=True)
     last_updated = models.DateTimeField(null=True, auto_now=True)
@@ -37,4 +38,3 @@ class Book(models.Model):
         ordering = ['-last_updated']
         verbose_name_plural = "Books"  # A human-readable name for the object, plural
         verbose_name = "Book"  # A human-readable name for the object, singular
-
