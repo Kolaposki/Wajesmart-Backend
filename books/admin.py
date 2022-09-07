87baeec3846b29a91fba16ffc7af8dc1df5b9b29
Kolaposki <oshodikolapo@gmail.com>
2022-09-07 14:43:59 +0100

    :sparkles: Create book and author admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .models import Book, Author
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'isbn', 'author', 'created_at',)
+    list_display_links = ('id', 'name',)
+    search_fields = ('name',)
+    list_per_page = 50
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'created_at',)
+    list_display_links = ('id', 'first_name',)
+    search_fields = ('first_name',)
+    list_per_page = 50
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.unregister(Group)  # remove Group objects from admin page:  under[Authentication and Authorization]
