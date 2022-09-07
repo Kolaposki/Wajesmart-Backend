23209d17911729f8d394f48e904e9337a819a2bb
Kolaposki <oshodikolapo@gmail.com>
2022-09-07 13:43:00 +0100

    :sparkles: Create book and author serializers

--- books/api/serializers.py
+++ books/api/serializers.py
@@ -1,1 +1,17 @@
 from rest_framework import serializers
+
+from books.models import Author, Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    author_id = serializers.IntegerField()
+
+    class Meta:
+        model = Book
+        fields = "__all__"
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = "__all__"
